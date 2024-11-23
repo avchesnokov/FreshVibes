3889965cc7f6329187be09a7c2837160862e5843
avchesnokov <chesnokov.a.chish@gmail.com>
2024-11-23 20:15:19 +0300

    Login page & main screen

--- app/src/main/java/com/example/freshvibes/MainActivity.java
+++ app/src/main/java/com/example/freshvibes/MainActivity.java
@@ -5,7 +5,11 @@ import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,8 +18,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
-    Button VK_button;
+ImageView login_button;
+ImageView titleScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +34,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        VK_button = findViewById(R.id.VK_button);
-        VK_button.setOnClickListener(v -> gotoUrl("https://www.google.com"));
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(v -> {});
+
+        titleScreen = findViewById(R.id.titleScreen);
+        fadeOutAndHideImage(titleScreen);
+
     }
     void gotoUrl(String link){
         try{
@@ -38,4 +49,38 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"No website found.", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    private void fadeOutAndHideImage(final ImageView img)
+    {
+        Animation wait = new AlphaAnimation(1, 1);
+        wait.setDuration(2000);
+        wait.setAnimationListener(new Animation.AnimationListener()
+        {
+            public void onAnimationEnd(Animation animation)
+            {
+                Animation fadeOut = new AlphaAnimation(1, 0);
+                fadeOut.setInterpolator(new AccelerateInterpolator());
+                fadeOut.setDuration(200);
+                fadeOut.setAnimationListener(new Animation.AnimationListener()
+                {
+                    public void onAnimationEnd(Animation animation)
+                    {
+                        img.setVisibility(View.GONE);
+                    }
+                    public void onAnimationRepeat(Animation animation) {}
+                    public void onAnimationStart(Animation animation) {}
+                });
+                img.startAnimation(fadeOut);
+            }
+            public void onAnimationRepeat(Animation animation) {}
+            public void onAnimationStart(Animation animation) {}
+        });
+        img.startAnimation(wait);
+
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/freshvibes/SecondActivity.java
@@ -0,0 +1,4 @@
+package com.example.freshvibes;
+
+public class SecondActivity {
+}
